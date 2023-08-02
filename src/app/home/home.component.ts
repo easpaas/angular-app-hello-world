987fe89ea061c5559ebe30a23a44570dde914db8
Evan Aspaas <aspaasevan@gmail.com>
2023-08-02 18:00:03 +0200

    update: init housingLocation data binding
    Signed-off-by: Evan Aspaas <aspaasevan@gmail.com>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,27 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { HousingLocationComponent } from "../housing-location/housing-location.component";
-import { HousingLocation }  from '../housinglocation';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
-    HousingLocationComponent,
+    HousingLocationComponent
   ],
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button type="button">Search</button>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
       </form>
     </section>
     <section class="results">
-      <app-housing-location></app-housing-location>
+      <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
     </section>
   `,
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 
 export class HomeComponent {
