2e01ddd6f9134f96bf0f68d4c401db96773f9540
Evan Aspaas <aspaasevan@gmail.com>
2023-08-02 16:20:20 +0200

    update: import new HousingLocationComponent
    Signed-off-by: Evan Aspaas <aspaasevan@gmail.com>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
+    HousingLocationComponent,
   ],
   template: `
     <section>
       <form>
         <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
+        <button class="prim ary" type="button">Search</button>
       </form>
+    </section>
+    <section>
+      <app-housing-location />
     </section>
   `,
   styleUrls: ['./home.component.css']
